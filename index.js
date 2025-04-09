e3195951a8ccddef19f3899b7e5fa314875ce965
Berkay Efe <69393574+Sultan-papagani@users.noreply.github.com>
2025-04-09 21:48:58 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+function linkimiz(){
+  document.location='https://linktr.ee/karabukdkke'
+  target="_blank"
+}
+
+
+window.addEventListener('scroll', function() {
+  const page1 = document.querySelector('.logo');
+  const logo = document.querySelector('.logo');
+  const scrollPosition = window.scrollY;
+  
+  // When scrolling reaches the height of page1, make it sticky
+  if (scrollPosition > page1.offsetHeight) {
+      page1.classList.add('sticky');
+      
+  } else {
+      page1.classList.remove('sticky');
+      
+      // Optional: Smooth scaling as you scroll
+      const scale = Math.max(0.5, 1 - scrollPosition / 500);
+      logo.style.transform = `translate(50%, -100%) scale(${scale})`;
+  }
+});
